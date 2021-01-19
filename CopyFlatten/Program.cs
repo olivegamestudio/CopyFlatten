a7d49d08285e8beddfbd225e2fb77000aa056bdc
Simon Jefferies <simon@olivegame.studio>
2021-01-19 17:52:17 +0000

    Imported.

---
+++ CopyFlatten/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace CopyFlatten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = args[0];
+            string dest = args[1];
+            string pattern = args[2];
+
+            foreach (string filename in Directory.GetFiles(source, pattern, SearchOption.AllDirectories))
+            {
+                string filenameOnly = Path.GetFileName(filename);
+                File.Copy(filename, Path.Combine(dest, filenameOnly), true);
+            }
+        }
+    }
+}
